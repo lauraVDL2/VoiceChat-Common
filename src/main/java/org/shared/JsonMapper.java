41ee02cafa70a1d8ee1b67cac5e26332ea68abf6
lauraVDL2 <laura.vanderderlinden@hotmail.com>
2025-08-24 11:11:50 +0200

    conversation display

---
+++ src/main/java/org/shared/JsonMapper.java
@@ -0,0 +1,15 @@
+package org.shared;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonMapper {
+
+    private static ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
+
+    public static ObjectMapper getJsonMapper() {
+        return objectMapper;
+    }
+}
+
