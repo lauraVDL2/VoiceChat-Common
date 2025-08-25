c67b515c3c5efa36f7f292f8b041ef379730e3e9
lauraVDL2 <laura.vanderderlinden@hotmail.com>
2025-08-25 21:59:53 +0200

    use emailaddress for map

--- src/main/java/org/shared/JsonMapper.java
+++ src/main/java/org/shared/JsonMapper.java
@@ -1,6 +1,7 @@
 package org.shared;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.shared.entity.User;
@@ -9,7 +10,7 @@ public class JsonMapper {
 
     private static ObjectMapper objectMapper = new ObjectMapper()
             .registerModule(new JavaTimeModule())
-            .registerModule(new SimpleModule().addKeyDeserializer(User.class, new UserKeyDeserializer()));
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
     public static ObjectMapper getJsonMapper() {
         return objectMapper;
