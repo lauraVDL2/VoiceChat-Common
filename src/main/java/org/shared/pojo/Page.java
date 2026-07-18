5c2165cc753f05349a5e471071d0e4db18c4a784
lauraVDL2 <laura.vanderderlinden@hotmail.com>
2026-07-18 09:17:23 +0200

    enhance Page pojo

--- src/main/java/org/shared/pojo/Page.java
+++ src/main/java/org/shared/pojo/Page.java
@@ -1,6 +1,32 @@
 package org.shared.pojo;
 
 public class Page {
-    public int offset = 0;
-    public int limit = 20;
+    private int offset;
+    private int limit;
+
+    public Page() {
+        this.offset = 0;
+        this.limit = 20;
+    }
+
+    public Page(int offset, int limit) {
+        this.offset = offset;
+        this.limit = limit;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 }
